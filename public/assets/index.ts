0b9ce01894232b8223899c182440fea328fee15c
Devarsh <devarsh.dk@gmail.com>
2024-01-02 13:53:23 +0530

    commit 3

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -2,6 +2,6 @@ import logo from "./images/logo.png";
 import profileImg from "./images/profile.jpg";
 import amazonImg from "./images/amazonImg.png";
 import cyberImg from "./images/cyberBlog.png";
-import noorShop from "./images/noorShop.png";
+import noorShop from "./images/noorshop.png";
 
 export { logo, profileImg, amazonImg, cyberImg, noorShop };
